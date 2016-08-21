db6da2472a965c1e943fcbd9ea8d516d2703014b
HK Kahng <hk@router-rooter.com>
2016-08-21 08:31:20 -0700

    add function to populate paragraphs via hipsterjesus api

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,13 @@
 
 // Front End Code
 $(document).ready (function(){
-
+  $.getJSON('http://hipsterjesus.com/api/?paras=1', function(data) {
+            $('#column1').html( data.text );
+        });
+  $.getJSON('http://hipsterjesus.com/api/?paras=1', function(data) {
+            $('#column2').html( data.text );
+        });
+  $.getJSON('http://hipsterjesus.com/api/?paras=1', function(data) {
+            $('#column3').html( data.text );
+        });
 });
